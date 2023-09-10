2dc16e8badd7cd61633eb4830dcfa76d2824cc2c
hettie-d <henr33@gmail.com>
2023-09-10 15:58:47 -0500

    load all edit

--- sql/_load_all.sql
+++ sql/_load_all.sql
@@ -1,6 +1,6 @@
-drop schema if exists norm_gen cascade;
-create schema norm_gen;
-set search_path to norm_gen;
+drop schema if exists norm cascade;
+create schema norm;
+set search_path to norm;
 \ir create_norm_gen_tables.sql;
 \ir process_schema_pkg.sql
 \ir ts_all.sql
