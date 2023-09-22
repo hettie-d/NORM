507a5d8fd6db14dfce64ec09c4b6144241d2a23f
“Boris <borisnov@acm.org>
2023-09-22 09:48:30 +0300

    load all sql

--- sql/_load_all.sql
+++ sql/_load_all.sql
@@ -1,6 +1,6 @@
-drop schema if exists norm cascade;
-create schema norm;
-set search_path to norm;
+drop schema if exists norm_gen cascade;
+create schema norm_gen;
+set search_path to norm_gen;
 \ir create_norm_gen_tables.sql;
 \ir process_schema_pkg.sql
 \ir ts_all.sql
@@ -8,10 +8,7 @@ set search_path to norm;
 \ir build_return_type_pkg.sql
 \ir build_select_pkg.sql
 \ir build_to_db.sql
-\ir generate_select_by_id.sql
-\ir generate_search_generic.sql
+\ir create_generated_types.sql
+\ir generate_from_db.sql
+\ir generate_select_by_ids.sql
 \ir generate_to_db_function.sql
-
-
-
-
